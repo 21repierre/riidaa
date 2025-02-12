52e7e1a2972fe45cdb69c9baec305d05e8c0661c
repierre <me@pierrebdvl.fr>
2025-02-12 13:31:52 +0900

    Initial Commit

---
+++ redaaTests/redaaTests.swift
@@ -0,0 +1,16 @@
+//
+//  redaaTests.swift
+//  redaaTests
+//
+//  Created by Pierre on 2025/02/12.
+//
+
+import Testing
+
+struct redaaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
